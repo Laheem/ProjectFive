cb2050c9ea53a59221e9963b69b82f478d8176f7
Laheem <Laheemgit@tutanota.com>
2020-05-06 23:30:56 +0100

    Adding Weapons Manager blueprint.

---
+++ ProjectFive/WeaponsManager/WeaponsManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using GTANetworkAPI;
+
+namespace ProjectFive.WeaponsManager
+{
+    class WeaponsManager : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void onWeaponsManagerResourceStart()
+        {
+            NAPI.Util.ConsoleOutput("[WEAPONS MANAGER] Weapons Manager has booted...");
+        } 
+
+
+    }
+}
