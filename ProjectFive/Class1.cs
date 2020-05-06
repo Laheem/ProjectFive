90920692d83d04c4b6be732e23b20665a15d273e
Laheem <Laheemgit@tutanota.com>
2020-05-06 23:18:26 +0100

    Add project files.

---
+++ ProjectFive/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace ProjectFive
+{
+    public class Class1 : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStart()
+        {
+            NAPI.Util.ConsoleOutput("fucking finally.");
+        }
+
+        public Class1()
+        {
+            Console.WriteLine("WOOO");
+        }
+    }
+}
