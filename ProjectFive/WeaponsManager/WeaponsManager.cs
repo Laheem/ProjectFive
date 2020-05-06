c563acd10bc831973cdd706c22c98451265de843
Laheem <Laheemgit@tutanota.com>
2020-05-06 23:55:57 +0100

    Added basic weapons command.

--- ProjectFive/WeaponsManager/WeaponsManager.cs
+++ ProjectFive/WeaponsManager/WeaponsManager.cs
@@ -12,7 +12,21 @@ namespace ProjectFive.WeaponsManager
         public void onWeaponsManagerResourceStart()
         {
             NAPI.Util.ConsoleOutput("[WEAPONS MANAGER] Weapons Manager has booted...");
+
         } 
+
+        [Command("giveweapon", Alias = "givewep")]
+        public void giveWeapon(Player player, String weaponName )
+        {
+            WeaponHash targetWeapon = NAPI.Util.WeaponNameToModel(weaponName);
+            if(targetWeapon == 0)
+            {
+                NAPI.Chat.SendChatMessageToPlayer(player, "That's an invalid weapon name.");
+            } else
+            {
+                NAPI.Player.GivePlayerWeapon(player, targetWeapon, 9999);
+            }
+        }
 
 
     }
