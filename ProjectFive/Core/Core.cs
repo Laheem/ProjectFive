8be7ddea4d3b7a10b5132abf0ceb0d53781654cd
Laheem <Laheemgit@tutanota.com>
2020-05-08 19:59:09 +0100

    Adding new commands and trying to fix config issues.

--- ProjectFive/Core/Core.cs
+++ ProjectFive/Core/Core.cs
@@ -10,5 +10,45 @@ namespace ProjectFive
         {
             NAPI.Util.ConsoleOutput("[CORE] Server booted up succesfully...");
         }
+
+        [Command("health", GreedyArg = true, Alias = "test")]
+        public void giveHealth(Player player, String hp)
+        {
+            int hpNumber;
+            if(int.TryParse(hp, out hpNumber))
+            {
+                player.Health = hpNumber;
+            } else
+            {
+                NAPI.Chat.SendChatMessageToPlayer(player, "That's not a number.");
+            }
+        }
+
+        [Command("armour", GreedyArg = true)]
+        public void giveArmour(Player player, String armour)
+        {
+            int armourNumber;
+            if (int.TryParse(armour, out armourNumber))
+            {
+                player.Armor = armourNumber;
+            } else
+            {
+                NAPI.Chat.SendChatMessageToPlayer(player, "That's not a number.");
+            }
+        }
+
+        [Command("changemodel", GreedyArg = true)]
+        public void changeModel(Player player, String name)
+        {
+            var modelHash = NAPI.Util.PedNameToModel(name);
+
+            if(modelHash == 0)
+            {
+                NAPI.Chat.SendChatMessageToPlayer(player, "That's not a valid player model.");
+            } else
+            {
+                NAPI.Player.SetPlayerSkin(player, modelHash);
+            }
+        }
     }
 }
---
+++ ProjectFiveClient/Client/VehicleManager/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectFiveClient.Client.VehicleManager
+{
+    class Class1
+    {
+    }
+}
