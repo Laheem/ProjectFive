ecbb5b563f9303ee072b384ff2bdff77ddab4b5f
Laheem <Laheemgit@tutanota.com>
2020-05-08 03:43:21 +0100

    Fully working End to End with Native UI.

--- ProjectFive/WeaponsManager/WeaponsManager.cs
+++ ProjectFive/WeaponsManager/WeaponsManager.cs
@@ -22,20 +22,39 @@ namespace ProjectFive.WeaponsManager
         [Command("giveweapon", Alias = "givewep")]
         public void GiveWeapon(Player player, String weaponName )
         {
+            NAPI.Util.ConsoleOutput("I'm being hit....");
+
             WeaponHash targetWeapon = NAPI.Util.WeaponNameToModel(weaponName);
             if(targetWeapon == 0)
             {
                 NAPI.Chat.SendChatMessageToPlayer(player, "That's an invalid weapon name.");
             } else
             {
-                NAPI.Player.GivePlayerWeapon(player, targetWeapon, 9999);
+                NAPI.Player.GivePlayerWeapon(player, targetWeapon, 999);
             }
         }
+
 
         [Command("weapon", Alias = "wep")]
         public void CreateWeaponList(Player player)
         {
-            NAPI.ClientEvent.TriggerClientEvent(player, "weaponList");
+
+            var data = GetTargetWeaponList("melee");
+            List<string> weps = JsonConvert.DeserializeObject<List<string>>(data.ToString());
+            Console.WriteLine(weps.ToString());
+
+
+
+            NAPI.ClientEvent.TriggerClientEvent(player, "weaponList",
+                GetTargetWeaponList("melee"),
+                GetTargetWeaponList("handguns"),
+                GetTargetWeaponList("smg"),
+                GetTargetWeaponList("shotguns"),
+                GetTargetWeaponList("assault_rifles"),
+                GetTargetWeaponList("sniper_rifles"),
+                GetTargetWeaponList("heavy_weapons"),
+                GetTargetWeaponList("throwables")
+                );
         }
 
         [Command("tp", Alias = "teleport", GreedyArg = true)]
@@ -58,6 +77,27 @@ namespace ProjectFive.WeaponsManager
             GiveWeapon(player, weaponName);
         }
 
+
+
+
+        String GetTargetWeaponList(string weaponName)
+        {
+            List<String> allWeaponNames = new List<String>();
+            using (StreamReader r = new StreamReader("weapons.json"))
+            {
+                string json = r.ReadToEnd();
+                var values = JObject.Parse(json);
+                var selectedList = values[weaponName];
+
+                foreach (JProperty item in selectedList)
+                {
+                    allWeaponNames.Add(item.Name);
+                }
+
+                return NAPI.Util.ToJson(allWeaponNames);
+            }
+
+        }
 
 
     }
---
+++ ProjectFiveClient/Client/WeaponsManager/WeaponsUI.cs
@@ -0,0 +1,95 @@
+﻿using MessagePack;
+using RAGE;
+using RAGE.Game;
+using RAGE.NUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Drawing;
+
+namespace WeaponsManager
+{
+    class WeaponsUI : RAGE.Events.Script
+    {
+        private MenuPool _menuPool;
+
+        public WeaponsUI()
+        {
+            _menuPool = new MenuPool();
+            RAGE.Events.Add("weaponList", generateWeaponsUI);
+            RAGE.Events.Tick += Tick;
+
+
+
+            void generateWeaponsUI(object[] args)
+            {
+                _menuPool = new MenuPool();
+                UIMenu mainMenu = new UIMenu("Weapons List", "Select a type of weapon you want", new Point(900,50));
+
+                UIMenu meleeMenu = _menuPool.AddSubMenu(mainMenu, "Melee", new Point(900,50));
+                UIMenu pistolMenu = _menuPool.AddSubMenu(mainMenu, "Pistols", new Point(900, 50));
+                UIMenu smgMenu = _menuPool.AddSubMenu(mainMenu, "Sub Machine Guns", new Point(900, 50));
+                UIMenu shotgunMenu = _menuPool.AddSubMenu(mainMenu, "Shotguns", new Point(900, 50));
+                UIMenu rifleMenu = _menuPool.AddSubMenu(mainMenu, "Rifles", new Point(900, 50));
+                UIMenu heavyMenu = _menuPool.AddSubMenu(mainMenu, "Heavy Weapons", new Point(900, 50));
+                UIMenu sniperMenu = _menuPool.AddSubMenu(mainMenu, "Sniper Rifles", new Point(900, 50));
+                UIMenu throwableMenu = _menuPool.AddSubMenu(mainMenu, "Throwables", new Point(900, 50));
+
+                _menuPool.Add(mainMenu);
+
+                var bytes = RAGE.Util.MsgPack.ConvertFromJson((string)args[0]);
+                List<String> weps = RAGE.Util.MsgPack.Deserialize<List<String>>(bytes);
+
+
+         
+
+                AddWeaponButtons(ConvertJsonToList((string)args[0]), meleeMenu);
+                AddWeaponButtons(ConvertJsonToList((string)args[1]), pistolMenu);
+                AddWeaponButtons(ConvertJsonToList((string)args[2]), smgMenu);
+                AddWeaponButtons(ConvertJsonToList((string)args[3]), shotgunMenu);
+                AddWeaponButtons(ConvertJsonToList((string)args[4]), rifleMenu);
+                AddWeaponButtons(ConvertJsonToList((string)args[5]), sniperMenu);
+                AddWeaponButtons(ConvertJsonToList((string)args[6]), heavyMenu);
+                AddWeaponButtons(ConvertJsonToList((string)args[7]), throwableMenu);
+
+                _menuPool.RefreshIndex();
+                mainMenu.Visible = true;
+
+            }
+
+
+            void Tick(List<Events.TickNametagData> nametags)
+            {
+                _menuPool.ProcessMenus();
+            }
+
+        }
+
+   
+
+        void AddWeaponButtons(List<String> listOfWeapons, UIMenu targetMenu)
+        {
+            foreach(String weapon in listOfWeapons)
+            {
+                UIMenuItem weaponItem = new UIMenuItem(weapon);
+                weaponItem.Activated += WeaponItem_Activated;
+                targetMenu.AddItem(weaponItem);
+            }
+        }
+
+        private void WeaponItem_Activated(UIMenu sender, UIMenuItem selectedItem)
+        {
+            RAGE.Events.CallRemote("giveWeapon", selectedItem.Text);
+        }
+
+        private List<String> ConvertJsonToList(String json)
+        {
+            var bytes = RAGE.Util.MsgPack.ConvertFromJson(json);
+            return RAGE.Util.MsgPack.Deserialize<List<String>>(bytes);
+
+        }
+    }
+
+        
+}
