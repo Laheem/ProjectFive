c6213be9df5a23488c2a6eb7067134a1d0816eab
Laheem <Laheemgit@tutanota.com>
2020-05-08 14:03:17 +0100

    Working vehicle implementation via /vehicle command.

---
+++ ProjectFive/VehicleManager/VehicleManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+
+namespace ProjectFive.VehicleManager
+{
+    class VehicleManager : Script
+    {
+
+
+        [ServerEvent(Event.ResourceStart)]
+        private void OnVehicleManagerResourceStart()
+        {
+            NAPI.Util.ConsoleOutput("[VEHICLE MANAGER] Vehicle Manager has successfully started!");
+        }
+
+
+        [Command("vehicle", Alias = "veh", GreedyArg = true)]
+        private void SpawnTargetVehicle(Player player, String vehicleName)
+        {
+            VehicleHash targetVehicle = NAPI.Util.VehicleNameToModel(vehicleName);
+            if(targetVehicle == 0)
+            {
+                NAPI.Chat.SendChatMessageToPlayer(player, "That looks like an invalid car name.");
+            }
+            NAPI.Vehicle.CreateVehicle(targetVehicle, player.Position, new float(), 255, 255);
+        }
+    }
+}
