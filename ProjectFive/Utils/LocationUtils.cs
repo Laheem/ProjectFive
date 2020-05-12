ede3f05184f651178bee4d0a07282bab56fcd307
Laheem <Laheemgit@tutanota.com>
2020-05-12 20:31:55 +0100

    Removing writelines, adding logging functionality.

---
+++ ProjectFive/Utils/LocationUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using GTANetworkAPI;
+
+namespace ProjectFive.Utils
+{
+    class LocationUtils : Script
+    {
+        [Command("loc")]
+        public void getPlayerLocation(Player player, String send)
+        {
+            NAPI.Chat.SendChatMessageToPlayer(player, player.Position.ToString());
+            
+            if(send != null && send.ToLower() == "send")
+            {
+                using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter("locations.txt", true))
+                {
+                    file.WriteLine(player.Position.ToString() + "\n");
+                }
+            }
+
+        }
+    }
+}
---
+++ ProjectFiveClient/Client/Logger/LocationLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RAGE;
+
+namespace ProjectFiveClient.Client.Logger
+{
+    class LocationLogger : Events.Script
+    {
+        public LocationLogger()
+        {
+            RAGE.Events.Add("logloc", PrintToLocationFile);
+        }
+
+        private void PrintToLocationFile(object[] args)
+        {
+            
+        }
+    }
+}
