3ae7bc86bd51b4325e4446b723d982438e0aefd5
Laheem <Laheemgit@tutanota.com>
2020-05-12 20:47:56 +0100

    Removing extra logging and simplifying using statement.

--- ProjectFive/Utils/LocationUtils.cs
+++ ProjectFive/Utils/LocationUtils.cs
@@ -13,16 +13,12 @@ namespace ProjectFive.Utils
         {
 
             NAPI.Chat.SendChatMessageToPlayer(player, player.Position.ToString());
-            NAPI.Chat.SendChatMessageToPlayer(player, comment);
-
-
+    
             if (send != null && send.ToLower() == "send")
             {
-                using (System.IO.StreamWriter file =
-                    new System.IO.StreamWriter("locations.txt", true))
-                {
-                    file.WriteLine($"{player.Position.ToString()} - {comment} from {player.Name}");
-                }
+                using System.IO.StreamWriter file =
+                    new System.IO.StreamWriter("locations.txt", true);
+                file.WriteLine($"{player.Position} - {comment} from {player.Name}");
             }
 
         }
