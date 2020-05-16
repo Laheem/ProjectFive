1032d109dceaa3ebdcf50b2f8f5f093ad12b7ef2
Laheem <Laheemgit@tutanota.com>
2020-05-16 19:41:28 +0100

    Mapping parsing foundations.

---
+++ ProjectFive/MappingManager/MappingParser.cs
@@ -0,0 +1,19 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectFive.MappingManager
+{
+    class MappingParser : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void LoadMapping()
+        {
+            NAPI.Util.ConsoleOutput("[MAPPING MANAGER] Now parsing all available mapping... This may take a while.");
+
+        }
+
+
+    }
+}
---
+++ ProjectFive/MappingManager/dto/MappingObject.cs
@@ -0,0 +1,16 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectFive.MappingManager.dto
+{
+    class MappingObject
+    {
+        float X { get;}
+        float Y { get;}
+        float Z { get; }
+        float dimension { get; } = 0;
+
+    }
+}
